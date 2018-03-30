9407048b386e96d8056f96c0f3da3374c64a4aa7
Anthony Curtis <tonydc1997@gmail.com>
2018-03-30 08:04:53 -0400

    Change element properties

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const $odd = $("a:odd");
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
-
+$secureLinks.attr('target', '_blank');
+$pdfs.attr('download', true);
